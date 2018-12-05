78714c4a25e23070fece834e1d30093bc663c135
Riccardo87 <riccardo.vancini@gmail.com>
2018-12-05 13:15:25 +0100

    senza fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Jumbotron,
+  Container,
+  Row,
+  Col,
+} from 'reactstrap';
+
+import Post from './component/post'
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <Navbar color="danger" light expand="md">
+
+          <NavbarBrand href="/">
+            <b>Riccardo Blog</b>
+          </NavbarBrand>
+
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+
+            <Nav className="ml-auto" navbar>
+
+              <NavItem>
+                <NavLink href="/components/">Post</NavLink>
+              </NavItem>
+
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  User
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Comment
+                  </DropdownItem>
+                  <DropdownItem>
+                    Photos
+                  </DropdownItem>
+                  <DropdownItem>
+                    Todos
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+
+        <Container sm="12" md={{ size: 6, offset: 3 }}>
+          <Jumbotron fluid >
+
+      <Post />
+              
+          </Jumbotron>
+        </Container>
+
       </div>
     );
   }
---
+++ src/component/post.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Post extends Component {
+    constructor(props) {
+      super(props);
+  
+    
+
+    }
+
+
+    render() {
+        return (
+            <div className="post">
+
+                dddddd
+    
+                </div>
+        );
+    }
+}
+
+export default Post;
